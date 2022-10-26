da6d08aa87f899657f62f3ee6b112570f8733fe1
NGUYEN_CHIEN <nguyenchien.0790@gmail.com>
2022-10-26 14:39:29 +0700

    lan 1

---
+++ src/main/java/rikkei/academy/repository/IProductRepository.java
@@ -0,0 +1,11 @@
+package rikkei.academy.repository;
+
+import rikkei.academy.model.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends PagingAndSortingRepository<Product, Long> {
+
+}
+
